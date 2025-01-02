df454d48808d380d4a248d49e9c277058ffd73a2
John Gannaway <johngannaway1@yahoo.co.uk>
2025-01-02 23:43:05 +0100

    Added Convolutional and Flatten layer, constructed training sample for Iris

--- iris_solver.py
+++ iris_solver.py
@@ -1,9 +1,14 @@
 import numpy as np
+import pandas as pd
 
 from network import Network
 from fc_layer import FCLayer
 from activation_layer import ActivationLayer
 from utils import tanh, tanh_prime, mse, mse_prime
+
+data = pd.read_csv("./iris_dataset/Iris.csv")
+training_count = int(len(data.index)*9/10)
+training_data = data.sample(training_count)
 
 
 net = Network()
@@ -11,3 +16,5 @@ net.add(FCLayer(4, 16))
 net.add(ActivationLayer(tanh, tanh_prime))
 net.add(FCLayer(16, 3))
 net.add(ActivationLayer(tanh, tanh_prime))
+
+net.use(mse, mse_prime)
